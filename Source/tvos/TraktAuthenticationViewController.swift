0e0726f1a7bcbf82ef8a0c915f312782aadd9e6a
Dylan Elliott <dylan.elliott@outware.com.au>
2019-02-09 23:31:22 +1100

    ATV fix

--- Source/tvos/TraktAuthenticationViewController.swift
+++ Source/tvos/TraktAuthenticationViewController.swift
@@ -18,26 +18,26 @@ public class TraktAuthenticationViewController: UIViewController {
     internal weak var delegate: TraktAuthViewControllerDelegate!
     internal var trakt: Trakt!
 
-	var intervalTimer: NSTimer?
+	var intervalTimer: Timer?
 	var responseCode: GeneratedCodeResponse? {
 		didSet {
 			intervalTimer?.invalidate()
 			if responseCode != nil {
 				uriLabel?.text = responseCode!.verificationUrl
 				codeLabel?.text = responseCode!.userCode
-				intervalTimer = NSTimer.scheduledTimerWithTimeInterval(responseCode!.interval, target: self, selector: #selector(TraktAuthenticationViewController.poll(_:)), userInfo: nil, repeats: true)
+				intervalTimer = Timer.scheduledTimer(timeInterval: responseCode!.interval, target: self, selector: #selector(poll(timer:)), userInfo: nil, repeats: true)
 			}
 		}
 	}
 
-	public override func viewWillAppear(animated: Bool) {
+	public override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(true)
 
 		getNewCode()
-		qrImageView.image = UIImage(named: "TraktQRCode.png", inBundle: TraktAuthenticationViewController.bundle, compatibleWithTraitCollection: nil)
+		qrImageView.image = UIImage(named: "TraktQRCode.png", in: TraktAuthenticationViewController.bundle, compatibleWith: nil)
 	}
 
-	public override func viewWillDisappear(animated: Bool) {
+	public override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
 
 		intervalTimer?.invalidate()
@@ -46,34 +46,34 @@ public class TraktAuthenticationViewController: UIViewController {
 	private func getNewCode() {
 		activity.startAnimating()
 
-        TraktRequestGenerateCode(clientId: trakt.clientId).request(trakt) { [weak self] data, error in
+		TraktRequestGenerateCode(clientId: trakt.clientId).request(trakt: trakt) { [weak self] data, error in
             self?.responseCode = data
             self?.activity.stopAnimating()
             if data == nil {
-                let ac = UIAlertController(title: "Trakt Error", message: error?.localizedDescription, preferredStyle: .Alert)
-                ac.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
-                self?.presentViewController(ac, animated: true, completion: nil)
+				let ac = UIAlertController(title: "Trakt Error", message: error?.localizedDescription, preferredStyle: .alert)
+				ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+				self?.present(ac, animated: true, completion: nil)
             }
         }
 	}
 
-    static var bundle: NSBundle? {
-        return NSBundle(forClass: TraktAuthenticationViewController.self)
+	static var bundle: Bundle? {
+		return Bundle(for: TraktAuthenticationViewController.self)
     }
 
-	public func poll(timer: NSTimer) {
-		if responseCode?.expiresAt.compare(NSDate()) == .OrderedAscending || responseCode == nil {
+	@objc public func poll(timer: Timer) {
+		if responseCode?.expiresAt.compare(NSDate() as Date) == .orderedAscending || responseCode == nil {
 			timer.invalidate()
 			getNewCode()
 		} else {
 			activity.startAnimating()
 
-            TraktRequestPollDevice(trakt: trakt, deviceCode: responseCode!.deviceCode).request(trakt) { [weak self] token, error in
+			TraktRequestPollDevice(trakt: trakt, deviceCode: responseCode!.deviceCode).request(trakt: trakt) { [weak self] token, error in
                 self?.activity.stopAnimating()
                 if token != nil {
                     timer.invalidate()
-                    self?.trakt.saveToken(token!)
-                    self?.delegate?.TraktAuthViewControllerDidAuthenticate(self!)
+					self?.trakt.saveToken(token: token!)
+					self?.delegate?.TraktAuthViewControllerDidAuthenticate(controller: self!)
                 }
             }
 		}
